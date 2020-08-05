ed478b941617b4c541e322cc734c4a9dbac2e90b
Sam Pinkus <sgpinkus@gmail.com>
2020-08-05 16:02:29 +1000

    Improve emoticon matching regex.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const visit = require('unist-util-visit');
 const emoji = require('node-emoji');
 const emoticon = require('emoticon');
-
-const RE_EMOJI = /:\+1:|:-1:|:[\w-]+:/g;
-const RE_SHORT = /[$@|*'",;.=:\-)([\]\\/<>038BOopPsSdDxXzZ]{2,5}/g;
+const { RE_ONE_EMOTICON, RE_EMOTICON, RE_EMOJI } = require('./regexp');
 
 const DEFAULT_SETTINGS = {
     padSpaceAfter: false,
@@ -15,18 +13,6 @@ function plugin(options) {
     const pad = !!settings.padSpaceAfter;
     const emoticonEnable = !!settings.emoticon;
 
-    function getEmojiByShortCode(match) {
-        // find emoji by shortcode - full match or with-out last char as it could be from text e.g. :-), 
-        const iconFull = emoticon.find(e => e.emoticons.includes(match)); // full match
-        const iconPart = emoticon.find(e => e.emoticons.includes(match.slice(0, -1))); // second search pattern
-        const trimmedChar = iconPart ? match.slice(-1): '';
-        const addPad = pad ? ' ': '';
-        let icon = iconFull ?
-            iconFull.emoji + addPad: 
-            iconPart && (iconPart.emoji + addPad +  trimmedChar);
-        return icon || match;
-    }
-
     function getEmoji(match) {
         const got = emoji.get(match);
         if (pad && got !== match) {
@@ -35,12 +21,28 @@ function plugin(options) {
         return got;
     }
 
+    function getEmojiByShortCode(match) {
+        // find emoji by shortcode - full match or with-out last char as it could be from text e.g. :-),
+        const iconFull = emoticon.find(e => e.emoticons.includes(match)); // full match
+        const iconPart = emoticon.find(e => e.emoticons.includes(match.slice(0, -1))); // second search pattern
+        const trimmedChar = iconPart ? match.slice(-1): '';
+        const addPad = pad ? ' ': '';
+        let icon = iconFull ?
+            iconFull.emoji + addPad:
+            iconPart && (iconPart.emoji + addPad +  trimmedChar);
+        return icon || match;
+    }
+
+    function getEmojiByShortCodeOuter(match) {
+        return match.replace(RegExp(RE_ONE_EMOTICON, 'g'), getEmojiByShortCode);
+    }
+
     function transformer(tree) {
         visit(tree, 'text', function(node) {
             node.value = node.value.replace(RE_EMOJI, getEmoji);
-            
+
             if (emoticonEnable) {
-                node.value = node.value.replace(RE_SHORT, getEmojiByShortCode);
+                node.value = node.value.replace(RE_EMOTICON, getEmojiByShortCodeOuter);
             }
         });
     }
