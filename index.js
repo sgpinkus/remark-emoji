8c7e234ff8b2c327fe515343b524572c7f337cdc
Sam Pinkus <sgpinkus@gmail.com>
2020-10-03 15:49:38 +1000

    Replace node-emoji dep with codename-to-emoji (smaller, faster).

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 const visit = require('unist-util-visit');
-const emoji = require('node-emoji');
 const { replaceEmoticons } = require('emoticon-to-emoji');
-const emojiRegExp = /:\+1:|:-1:|:[\w-]+:/g;
-
+const { replaceCodeNames } = require('codename-to-emoji');
 
 const defaultSettings = {
   padSpaceAfter: false,
   emoticon: false
 };
 
-
 function plugin(options) {
   const settings = Object.assign({}, defaultSettings, options);
   const pad = !!settings.padSpaceAfter;
   const emoticonEnable = !!settings.emoticon;
 
-  function getEmoji(match) {
-    const got = emoji.get(match);
-    if (pad && got !== match) {
-      return got + ' ';
-    }
-    return got;
-  }
-
   function transformer(tree) {
     visit(tree, 'text', function(node) {
-      node.value = node.value.replace(emojiRegExp, getEmoji);
+      node.value = replaceCodeNames(node.value, pad);
       if (emoticonEnable) {
         node.value = replaceEmoticons(node.value, undefined, pad);
       }
@@ -33,6 +22,5 @@ function plugin(options) {
   }
   return transformer;
 }
-
 
 module.exports = plugin;
